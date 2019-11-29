def46d091ce39f66439804d2a71581bff3b026c7
Adam Ochotnicki <adam.ochotnicki@mail.itsligo.ie>
2019-11-29 00:42:23 +0000

    create account button

--- lib/widgets/logIn.dart
+++ lib/widgets/logIn.dart
@@ -16,8 +16,6 @@ class _LogInState extends State<LogIn> {
     if (enteredEmail.isEmpty || enteredPassword.isEmpty) {
       return;
     }
-
-
   }
 
   @override
@@ -27,27 +25,42 @@ class _LogInState extends State<LogIn> {
       child: Center(
         child: Container(
           padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              onSubmitted: (_) => submitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              controller: passwordController,
-              onSubmitted: (_) => submitData(),
-            ),
-            RaisedButton(
-              child: Text('Log In'),
-              textColor: Theme.of(context).primaryColor,
-              onPressed: submitData,
-            ),
-          ],
-        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                onSubmitted: (_) => submitData(),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                controller: passwordController,
+                onSubmitted: (_) => submitData(),
+              ),
+              RaisedButton(
+                child: Text('Log In'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: submitData,
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Text('Not yet registered?'),
+                    RaisedButton(
+                      child: Text('Create an account'),
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
