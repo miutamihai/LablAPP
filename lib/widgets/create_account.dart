252e057c80fa50198a4c76ed0c45ec6a428c2084
Adam Ochotnicki <adam.ochotnicki@mail.itsligo.ie>
2020-01-29 20:00:15 +0000

    create account and log in keyboard overflow fixed

--- lib/widgets/create_account.dart
+++ lib/widgets/create_account.dart
@@ -36,17 +36,17 @@ class _CreateAccountState extends State<CreateAccount> {
     print(enteredPassword);
     print(confirmedPassword);
 
-  //   try {
-  //     final newUser = _auth.createUserWithEmailAndPassword(
-  //       email: enteredEmail,
-  //       password: enteredPassword,
-  //     );
-  //     if (newUser != null) {
-  //       // redirect to previous page
-  //     }
-  //   } catch (e) {
-  //     print(e);
-  //   }
+    //   try {
+    //     final newUser = _auth.createUserWithEmailAndPassword(
+    //       email: enteredEmail,
+    //       password: enteredPassword,
+    //     );
+    //     if (newUser != null) {
+    //       // redirect to previous page
+    //     }
+    //   } catch (e) {
+    //     print(e);
+    //   }
   }
 
   @override
@@ -56,49 +56,51 @@ class _CreateAccountState extends State<CreateAccount> {
       child: Center(
         child: Container(
           padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Email'),
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onSubmitted: (_) => submitData(),
-              ),
-              TextField(
-                //textAlign: TextAlign.center,
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'Password'),
-                controller: passwordController,
-                onSubmitted: (_) => submitData(),
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'Confirm password'),
-                controller: confirmationController,
-                onSubmitted: (_) => submitData(),
-              ),
-              RaisedButton(
-                child: Text('Create an account'),
-                textColor: Theme.of(context).primaryColor,
-                onPressed: submitData,
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              Center(
-                child: Column(
-                  children: [
-                    Text('Already have an account?'),
-                    RaisedButton(
-                      child: Text('Log in'),
-                      textColor: Theme.of(context).primaryColor,
-                      onPressed: () => widget.logIn(true), // go to logIn page
-                    ),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onSubmitted: (_) => submitData(),
                 ),
-              ),
-            ],
+                TextField(
+                  //textAlign: TextAlign.center,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  controller: passwordController,
+                  onSubmitted: (_) => submitData(),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Confirm password'),
+                  controller: confirmationController,
+                  onSubmitted: (_) => submitData(),
+                ),
+                RaisedButton(
+                  child: Text('Create an account'),
+                  textColor: Theme.of(context).primaryColor,
+                  onPressed: submitData,
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text('Already have an account?'),
+                      RaisedButton(
+                        child: Text('Log in'),
+                        textColor: Theme.of(context).primaryColor,
+                        onPressed: () => widget.logIn(true), // go to logIn page
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
