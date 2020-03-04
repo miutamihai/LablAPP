c041069d793eff728b04e7ed687f40cc6ad05799
Luis Miguel Panadero Cerdá <lmpc3@alu.ua.es>
2020-03-04 12:40:11 +0000

    Main.dart now with the debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LABL',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Acme',
         primarySwatch: Colors.green,
